488db70bc0bffb7dc817fcecb34251e0c7005503
Al-Amin Hossain <alaminmain@gmail.com>
2022-01-21 11:25:52 +0600

    Component tree

---
+++ src/components/MainComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './header/header';
+import Footer from './footer/Footer';
+import Body from './body/Body';
+const MainComponent = () => {
+    return (
+        <div>
+            <Header />
+            <Body />
+            <Footer />
+        </div>
+    );
+}
+export default MainComponent; 
