64704a15f91a209412e959e6982943f85d75f009
Al-Amin Hossain <alaminmain@gmail.com>
2022-01-22 09:04:08 +0600

    dish

--- src/components/body/Menu.js
+++ src/components/body/Menu.js
@@ -1,25 +1,41 @@
 import React, { Component } from "react";
 import DISHES from "../../data/dishes";
 import MenuItem from "./MenuItem";
+import DishDetail from "./DishDetail";
 
 class Menu extends Component {
     state = {
-        dishes: DISHES
+        dishes: DISHES,
+        selectecDish: null
+    }
+
+    onDishSelete = dish => {
+        console.log(dish);
+        this.setState({ selectecDish: dish });
     }
 
     render() {
 
         const menu = this.state.dishes.map(item => {
             return (
-                <MenuItem dish={item} key={item.id}/>
+                <MenuItem dish={item} key={item.id}
+                    DishSelete={() => this.onDishSelete(item)}
+                />
             )
         });
 
+        let dishDetail = null;
+        if (this.state.selectecDish != null) {
+            dishDetail = <DishDetail dish={this.state.selectecDish} />
+        }
         return (
             <div className="container">
                 <div className="row">
                     <div className="col-6">
                         {menu}
+                    </div>
+                    <div className="col-6">
+                        {dishDetail}
                     </div>
                 </div>
             </div>
